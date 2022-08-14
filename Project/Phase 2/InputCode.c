47dd09d716d0fd0f713c21285c05a1c5cd7f31fb
Hassankhand10 <61631195+Hassankhand10@users.noreply.github.com>
2022-08-14 15:37:26 +0500

    Update InputCode.c

--- Project/Phase 2/InputCode.c
+++ Project/Phase 2/InputCode.c
@@ -1,1 +1,29 @@
+int main() {
+    int x;
+    float y;
 
+    y = 23.55562;
+    x = 10;
+
+    /*
+     * // Hello
+           CC project 
+           Phase 02 
+Hello123123
+Overandout
+//   
+  */
+
+    // Print nahi karna haai 
+    
+    
+
+    while (x) {
+    x = x/10;
+    }
+
+    if (x == 0) {
+      y = 10;
+    }
+
+}
